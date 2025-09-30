1717ab3e303954fefc696d31eef73f0b1b5accfd
william samwaka <williamsamwaka@gmail.com>
2025-09-30 17:38:54 +0200

    created homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,34 +26,61 @@
         </div>
     </nav>
 
-    <main class="flex-1 container mx-auto px-4 py-8">
-            <div class="conatiner flex items-centre flex mx-auto ">
-                  <div class="card bg-base-100 w-96 shadow-sm">
-                        <figure class="px-10 pt-10">
-                            <img
-                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                            alt="Shoes"
-                            class="rounded-xl" />
-                        </figure>
-                        <div class="card-body items-center text-center">
-                            <h2 class="card-title">Customer</h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                            <div class="card-actions">
-                            <button class="btn btn-primary">Select Customer</button>
+    <main class="flex-1 container mx-auto mt-8 px-4 py-8">
+            <div class="hero bg-base-200 ">
+                <div class="hero-content text-center">
+                    <div class="max-w-md">
+                    <figure>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-car h-24 w-24 text-primary"><path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2"></path><circle cx="7" cy="17" r="2"></circle><path d="M9 17h6"></path><circle cx="17" cy="17" r="2"></circle></svg>
+                    </figure>
+                    <h1 class="text-5xl font-bold">Ad Auto Spares</h1>
+                    <p class="py-6">
+                        Your trusted partner for quality automotives spare parts and professional serivices 
+                    </p>
+                    </div>
+                </div>
+                
+           </div>
+            <div class="conatiner flex-1 gap-10 justify-center flex mt-8  px-4">
+                            <div class="card bg-base-100 w-96 shadow-sm">
+                                    <figure class="px-10 pt-10">
+                                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user h-20 w-20 text-primary"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                                    </figure>
+                                    <div class="card-body items-center text-center">
+                                        <h2 class="card-title">Local Staff</h2>
+                                        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                                        <div class="card-actions">
+                                        <button class="btn btn-primary">Select Customer</button>
+                                        </div>
+                                    </div>
                             </div>
-                        </div>
-                  </div>
-                <div class="card">
-                     <div class="card-body">
-                         <p>hello</p>
-                    </div>
-                </div>
-                <div class="card">
-                     <div class="card-body">
-                        <p>hello</p>
-                    </div>
-                </div>
+                            <div class="card bg-base-100 w-96 shadow-sm">
+                                    <figure class="px-10 pt-10">
+                                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings h-20 w-20 text-secondary"><path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+                                    </figure>
+                                    <div class="card-body items-center text-center">
+                                        <h2 class="card-title">Customer</h2>
+                                        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                                        <div class="card-actions">
+                                        <button class="btn btn-primary">Select Customer</button>
+                                        </div>
+                                    </div>
+                            </div>
+                            <div class="card bg-base-100 w-96 shadow-sm">
+                                    <figure class="px-10 pt-10">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-warehouse h-12 w-12 text-accent-foreground"><path d="M22 8.35V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V8.35A2 2 0 0 1 3.26 6.5l8-3.2a2 2 0 0 1 1.48 0l8 3.2A2 2 0 0 1 22 8.35Z"></path><path d="M6 18h12"></path><path d="M6 14h12"></path><rect width="12" height="12" x="6" y="10"></rect></svg>
+                                    </figure>
+                                    <div class="card-body items-center text-center">
+                                        <h2 class="card-title">Customer</h2>
+                                        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                                        <div class="card-actions">
+                                        <button class="btn btn-primary">Select Customer</button>
+                                        </div>
+                                    </div>
+                            </div>
+                
             </div>
+           
         
     </main>
 
